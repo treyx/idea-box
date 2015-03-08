15602cd3f64ea0d0ff815a266bf666ed21985f3f
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-08 17:05:07 -0600

    removed commented out let in spec - unnecessary

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe Idea, :type => :model do
   context "invalid attributes" do
-    # let(:idea) { Idea.new(:title "Idea1", :body "Idea Body") }
     it "is invalid without a title" do
       idea = Idea.new(body: "Idea Body")
       expect(idea).to_not be_valid
