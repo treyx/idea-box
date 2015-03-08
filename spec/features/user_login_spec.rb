816826d630f51ef1658b7d486eeac8c5604d33a2
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-03-08 15:30:13 -0600

    added spec for invalid password and changed login button name

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "User Logs In", :type => :feature do
     visit login_path
     fill_in "session[username]", with: user.username
     fill_in "session[password]", with: user.password
-    click_link_or_button "Log in"
+    click_link_or_button "Log In"
     expect(current_path).to eq(user_path(user))
     expect(page).to have_content("Welcome to IdeaBox, example")
   end
@@ -16,7 +16,17 @@ RSpec.feature "User Logs In", :type => :feature do
     visit login_path
     fill_in "session[username]", with: "User"
     fill_in "session[password]", with: user.password
-    click_link_or_button "Log in"
+    click_link_or_button "Log In"
+    expect(current_path).to eq(login_path)
+    expect(page).to have_content("Invalid Login")
+  end
+
+  scenario "with invalid password" do
+    user = User.create(username: "example", email_address: "example@example.com", password: "password")
+    visit login_path
+    fill_in "session[username]", with: user.username
+    fill_in "session[password]", with: "example"
+    click_link_or_button "Log In"
     expect(current_path).to eq(login_path)
     expect(page).to have_content("Invalid Login")
   end
